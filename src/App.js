7851f38ac911480dc1a5e3c60a0e7b8832494e98
Deusdete Ferreira <deuzinh2010@gmail.com>
2020-07-11 22:15:30 -0300

    Add Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
-import React from 'react';
+import React, { useEffect }from 'react';
 import Router from './router'
+import { PageView, initGA } from './components/Tracking';
 import { HelmetProvider } from 'react-helmet-async';
 import { Provider } from 'react-redux'
 import store from './redux/store'
+
+
 
 // import './css/bootstrap.min.css'
 import './css/typography.css'
@@ -11,6 +14,10 @@ import './css/responsive.css'
 import './css/slick.css'
 
 function App() {
+  initGA('UA-106127269-9');
+  useEffect(() => {
+    PageView();
+  })
   return (
     <Provider store={store}>
       <HelmetProvider>
