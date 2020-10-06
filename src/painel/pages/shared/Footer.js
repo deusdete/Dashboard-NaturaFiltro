333a1ae129931f8ffbb6a4fe9781911ccd0f61b2
Deusdete Ferreira <deuzinh2010@gmail.com>
2020-10-06 11:15:19 -0300

    Update Footer.js

--- src/painel/pages/shared/Footer.js
+++ src/painel/pages/shared/Footer.js
@@ -6,7 +6,7 @@ class Footer extends Component {
       <footer className="footer">
         <div className="container-fluid">
           <div className="d-sm-flex justify-content-center justify-content-sm-between">
-            <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2020 <a href="https://f4cil.com.br/" target="_blank" rel="noopener noreferrer">F4il Soluções em Software</a>. Todos os direitos reservados.</span>
+            <span className="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2020 <a href="http://losenvo.com/" target="_blank" rel="noopener noreferrer">Losenvo</a>.</span>
             <span className="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Foito com code & <i className="mdi mdi-heart text-danger"></i></span>
           </div>
         </div>
